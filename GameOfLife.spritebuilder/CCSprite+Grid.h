2208716e3d82c27e1675f1e5dfa0fd0025c2ec90
Kevin <kevincalixtro69@gmail.com>
2015-08-06 21:21:45 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/CCSprite+Grid.h
@@ -0,0 +1,14 @@
+//
+//  CCSprite+Grid.h
+//  GameOfLife
+//
+//  Created by Kevin Calixtro on 8/6/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+@interface CCSprite (Grid)
+
+@end
